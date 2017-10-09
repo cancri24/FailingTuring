36ce473b8b5ee639bab8329834ffc1474865a631
cancri24 <cirwindiehl@gmail.com>
2017-10-09 09:06:22 -0400

    Initial commit

---
+++ src/edu/cse/Main.java
@@ -0,0 +1,37 @@
+package edu.cse;
+
+import javafx.application.Application;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage theStage) throws Exception {
+        TextField sayThings = new TextField();
+        String questionbeans = "Can we talk about beans?";
+        String talkbeans = "I like beans.";
+        String botsaid = "BeanBot said: ";
+        String yousaid = "You said: ";
+        Text botTalk = new Text(botsaid + questionbeans);
+        Text youTalk = new Text(yousaid);
+        VBox textstuff = new VBox();
+        textstuff.getChildren().add(youTalk);
+        textstuff.getChildren().add(botTalk);
+        textstuff.getChildren().add(sayThings);
+        Scene theScene = new Scene(textstuff, 300, 300);
+
+        theStage.setScene(theScene);
+        theStage.setTitle("I am not a bot.");
+        theStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+}
