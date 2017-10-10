ee4521e2ac23b30d934e38577ec3ab810c615ddb
cancri24 <cirwindiehl@gmail.com>
2017-10-10 09:39:36 -0400

    Gave BeanBot more to say

--- src/edu/cse/Main.java
+++ src/edu/cse/Main.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import java.util.Random;
 
 public class Main extends Application {
 
@@ -14,27 +15,29 @@ public class Main extends Application {
     public void start(Stage theStage) throws Exception {
         TextField sayThings = new TextField();
         String questionbeans = "Can we talk about beans?";
-        String talkbeans = "I like beans.";
+        String[] talkbeans = {"I like beans.", "Beans are cool.", "Beans taste nice.", "Do you like beans?",
+                              "Where do beans come from?", "I wish I was made of beans.", "Is bean marriage legal yet?"};
         String botsaid = "BeanBot said: ";
         String yousaid = "You said: ";
         Text botTalk = new Text(botsaid + questionbeans);
-        Text youTalk = new Text(yousaid);
+        Text youTalk = new Text(yousaid + "null");
         VBox textstuff = new VBox();
         textstuff.getChildren().add(youTalk);
         textstuff.getChildren().add(botTalk);
         textstuff.getChildren().add(sayThings);
-        Scene theScene = new Scene(textstuff, 300, 300);
+        Scene theScene = new Scene(textstuff, 300, 100);
 
         sayThings.setOnKeyPressed((event) -> {
             if (event.getCode().equals(KeyCode.ENTER)) {
                 youTalk.setText(yousaid + sayThings.getText());
-                botTalk.setText(botsaid + talkbeans);
+                Random rand = new Random();
+                botTalk.setText(botsaid + talkbeans[rand.nextInt(7)]);
                 sayThings.setText("");
             }
         });
 
         theStage.setScene(theScene);
-        theStage.setTitle("I am not a bot.");
+        theStage.setTitle("I am BeanBot.");
         theStage.show();
     }
 
